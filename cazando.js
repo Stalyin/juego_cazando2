1091a8a6baefc6ab13471d1b456f4f11bf45f17a
Stalyin <jaden180802016@gmail.com>
2026-04-09 11:59:40 -0500

    feat: nuevo

--- cazando.js
+++ cazando.js
@@ -4,7 +4,13 @@ let context = canvas.getContext("2d")
 
 //8. Crear una función graficarGato (sin parámetros) que dibuje un rectángulo centrado en el canvas. 
 
+const ANCHO_DIBUJO = 50
+const ALTO_DIBUJO = 50
+
+const ANCHO = canvas.width / 2 - ANCHO_DIBUJO / 2
+const ALTO = canvas.height / 2 - ALTO_DIBUJO / 2
+
 function graficarGato (){
     context.fillStyle = "blue"
-    context.fillRect(250,250,25,25)
+    context.fillRect(ANCHO,ALTO,ANCHO_DIBUJO,ALTO_DIBUJO)
 }
